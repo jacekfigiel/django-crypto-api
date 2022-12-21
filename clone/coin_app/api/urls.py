f30f623bf72ece894d0d77d0eebf685b938b7028
jacekfigiel <jacekfigo1988@gmail.com>
2022-12-21 21:23:30 +0000

    sorting function added with links in html

--- clone/coin_app/api/urls.py
+++ clone/coin_app/api/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
-from .views import home, CryptoDetailView
+from .views import home, CryptoDetailView, home_price, home_symbol, home_percent_change_24h, home_percent_change_7d, home_market_cap, home_volume_24h, home_circulating_supply
 
 app_name = "coin_app"
 
 urlpatterns = [
     path("home/", home, name="home_view"),
+    path("home_price/", home_price, name="home_view_price"),
+    path("home_symbol/", home_symbol, name="home_view_symbol"),
+    path("home_percent_change_24h/", home_percent_change_24h, name="home_percent_change_24h"),
+    path("home_percent_change_7d/", home_percent_change_7d, name="home_percent_change_7d"),
+    path("home_market_cap/", home_market_cap, name="home_market_cap"),
+    path("home_volume_24h/", home_volume_24h, name="home_volume_24h"),
+    path("home_circulating_supply/", home_circulating_supply, name="home_circulating_supply"),
     path("detail_crypto/<int:pk>", CryptoDetailView.as_view(), name="detail_view")
 ]
--- clone/coin_app/api/views.py
+++ clone/coin_app/api/views.py
@@ -9,6 +9,40 @@ def home(request):
     crypto_dict = {"access_crypto": crypto_list}
     return render(request, "coin_app/home.html", context=crypto_dict)
 
+def home_symbol(request):
+    crypto_list = CryptoModel.objects.order_by("symbol")
+    crypto_dict = {"access_crypto": crypto_list}
+    return render(request, "coin_app/home.html", context=crypto_dict)
+
+def home_price(request):
+    crypto_list = CryptoModel.objects.order_by("-price")
+    crypto_dict = {"access_crypto": crypto_list}
+    return render(request, "coin_app/home.html", context=crypto_dict)
+
+def home_percent_change_24h(request):
+    crypto_list = CryptoModel.objects.order_by("-percent_change_24h")
+    crypto_dict = {"access_crypto": crypto_list}
+    return render(request, "coin_app/home.html", context=crypto_dict)
+
+def home_percent_change_7d(request):
+    crypto_list = CryptoModel.objects.order_by("-percent_change_7d")
+    crypto_dict = {"access_crypto": crypto_list}
+    return render(request, "coin_app/home.html", context=crypto_dict)
+
+def home_market_cap(request):
+    crypto_list = CryptoModel.objects.order_by("-market_cap")
+    crypto_dict = {"access_crypto": crypto_list}
+    return render(request, "coin_app/home.html", context=crypto_dict)
+
+def home_volume_24h(request):
+    crypto_list = CryptoModel.objects.order_by("-volume_24h")
+    crypto_dict = {"access_crypto": crypto_list}
+    return render(request, "coin_app/home.html", context=crypto_dict)
+
+def home_circulating_supply(request):
+    crypto_list = CryptoModel.objects.order_by("-circulating_supply")
+    crypto_dict = {"access_crypto": crypto_list}
+    return render(request, "coin_app/home.html", context=crypto_dict)
 
 class CryptoDetailView(DetailView):
     context_object_name = "crypto_model"
